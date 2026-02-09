559edc9cb93a11c60246cadcb0edbf907ad06da4
agrumel <82831460+mMyst@users.noreply.github.com>
2026-02-09 17:29:29 +0100

    new scripts to test a forced tiller number

---
+++ simulations/cn_wheat/tillering/wheat_default_2til.py
@@ -0,0 +1,100 @@
+from plantfusion.wheat_wrapper import Wheat_wrapper
+from plantfusion.light_wrapper import Light_wrapper
+from plantfusion.indexer import Indexer
+from plantfusion.planter import Planter
+
+import time
+import datetime
+import os
+
+
+def simulation(in_folder, out_folder,start_wheat=None, simulation_length=2500, 
+               write_geo=False, run_postprocessing=False, run_graphs=False, geostep=1):
+    try:
+        # Create target Directory
+        os.mkdir(os.path.normpath(out_folder))
+        print("Directory ", os.path.normpath(out_folder), " Created ")
+    except FileExistsError:
+        print("Directory ", os.path.normpath(out_folder), " already exists")
+
+    plants_name = "wheat"
+    index_log = Indexer(global_order=[plants_name], wheat_names=[plants_name])
+
+    N_fertilizations = {2016: 357143, 2520: 1000000}
+    tillers_replications = {"T1": 0.5, "T2": 0.5}
+    plant_density = {1: 250}
+    sky = "turtle46"
+    RERmax_vegetative_stages_example = {
+        "elongwheat": {
+            "RERmax": {5: 3.35e-06, 6: 2.1e-06, 7: 2.0e-06, 8: 1.83e-06, 9: 1.8e-06, 10: 1.65e-06, 11: 1.56e-06}
+        }
+    }
+    senescwheat_timestep = 1
+    light_timestep = 4
+
+    planter = Planter(generation_type="default", indexer=index_log, inter_rows=0.15, plant_density=plant_density)
+    
+    wheat = Wheat_wrapper(
+        in_folder=in_folder,
+        out_folder=out_folder,
+        planter=planter,
+        indexer=index_log,
+        external_soil_model=False,
+        nitrates_uptake_forced=False,
+        N_fertilizations=N_fertilizations,
+        tillers_replications=tillers_replications,
+        update_parameters_all_models=RERmax_vegetative_stages_example,
+        SENESCWHEAT_TIMESTEP=senescwheat_timestep,
+        LIGHT_TIMESTEP=light_timestep,
+    )
+
+    lighting = Light_wrapper(
+        lightmodel="caribu", 
+        out_folder=out_folder, 
+        sky=sky,
+        planter=planter, 
+        indexer=index_log,
+        writegeo=write_geo
+    )
+
+    current_time_of_the_system = time.time()
+
+    if start_wheat is not None: 
+        wheat.start_time=wheat.meteo[wheat.meteo['Date']==start_wheat].index[0]
+
+    for t in range(wheat.start_time, wheat.start_time+simulation_length, wheat.SENESCWHEAT_TIMESTEP):
+        if (t % light_timestep == 0) and (wheat.PARi_next_hours(t) > 0):
+            wheat_input, stems = wheat.light_inputs(planter)
+
+            if((write_geo==True) and (t%geostep*light_timestep == 0 )) : # save geometry as vtk file every geostep timestep
+                lighting.writegeo=True 
+            else:
+                lighting.writegeo=False
+
+            lighting.run(scenes=[wheat_input], day=wheat.doy(t), hour=wheat.hour(t), parunit="micromol.m-2.s-1", stems=stems)
+            wheat.light_results(energy=wheat.energy(t), lighting=lighting)
+
+        wheat.run(t)
+
+    execution_time = int(time.time() - current_time_of_the_system)
+    print("\n" "Simulation run in {}".format(str(datetime.timedelta(seconds=execution_time))))
+
+
+    wheat.end(run_postprocessing=run_postprocessing,run_graphs=run_graphs)
+
+
+if __name__ == "__main__":
+    in_folder = "inputs_fspmwheat"
+    out_folder = "outputs/cnwheat_default_tillers/2til"
+    start_wheat = None
+    simulation_length = 2500
+    write_geo = True
+    run_postprocessing=True
+    run_graphs=True
+    geostep = 10 
+
+
+    simulation(in_folder, out_folder,start_wheat,simulation_length, 
+                write_geo=write_geo, geostep = geostep,
+                run_postprocessing=run_postprocessing,
+                run_graphs=run_graphs)
